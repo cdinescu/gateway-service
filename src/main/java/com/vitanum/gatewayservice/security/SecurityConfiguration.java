8bc62e653f839be288b7db9bbe486c4b5724b22f
Cristina Dinescu <crina91@gmail.com>
2020-06-15 21:15:52 +0300

    Chnaged CORS filter location - the filter is based on ServerHttpSecurity
    Signed-off-by: Cristina Dinescu <crina91@gmail.com>

--- src/main/java/com/vitanum/gatewayservice/security/SecurityConfiguration.java
+++ src/main/java/com/vitanum/gatewayservice/security/SecurityConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
@@ -16,7 +18,6 @@ public class SecurityConfiguration {
         // @formatter:off
         http
                 .csrf()
-                .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
                 .authorizeExchange()
                 .anyExchange().authenticated()
@@ -25,7 +26,22 @@ public class SecurityConfiguration {
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
+
+        http.cors();
         return http.build();
         // @formatter:on
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOrigin("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
